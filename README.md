7ef7a6d1787280050249b664aa40f28ee7c6ccb3
adi ✨ <63885722+fjuton@users.noreply.github.com>
2023-08-08 21:44:14 +0200

    docs: add readme

---
+++ README.md
@@ -0,0 +1,18 @@
+# meowcloud
+
+customizable extension for https://soundcloud.com
+
+## features
+- skip all empty samples
+
+## building
+```sh
+git clone https://github.com/fjuton/meowcloud.git
+
+# install all node dependencies
+npm i
+npm run build
+# files should 
+# enjoy :3
+
+```
--- src/plugins/copyrightSeeker.ts
+++ src/plugins/copyrightSeeker.ts
@@ -7,6 +7,8 @@ import { registerSongEvent } from "../api/song";
 export const copyrightSeeker = () => {
     const SAMPLE_THRESHOLD = 10
     registerSongEvent("change:currentSound", async (song : {current?: any}) => {
+        console.log(`meowcloud - song switched`)
+        console.log(song)
         try {
             let body = await fetch(`${song?.current?.attributes?.waveform_url}`, {
                 method: "GET"
@@ -14,8 +16,20 @@ export const copyrightSeeker = () => {
     
             const waveform : {width?: number, height?: number, samples?: number[]} = await body.json()
             let samples = waveform?.samples?.slice(0, waveform.samples.findIndex(value => value > SAMPLE_THRESHOLD)) ?? []
-            console.log(samples.length, waveform.samples?.length, song?.current?.getFixedDuration())
-            song?.current?.seek((samples.length / (waveform.samples?.length ?? 1)) * song?.current?.getFixedDuration())
+            if (samples.length < 50) // copyrighted moments seems to have more than that
+                return
+
+            // wait until it starts playing so we don't get overriden by the actual player?
+            await new Promise((resolve) => {
+                const waitForSong = () => {
+                    if (song?.current?.getCurrentSound().isPlaying()) return resolve({})
+                    setTimeout(waitForSong, 500);
+                }
+
+                waitForSong()
+            });
+
+            song?.current?.getCurrentSound().seek((samples.length / (waveform.samples?.length ?? 1)) * song?.current?.getFixedDuration())
         } catch (err) {
             console.error("meowcloud - couldn't fetch waveform :3")
         }
