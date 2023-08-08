fc634ded3ad81cc1b487e30f8da7660273f697b5
adi ✨ <63885722+fjuton@users.noreply.github.com>
2023-08-08 21:04:05 +0200

    fix: change sample threshold

--- src/plugins/copyrightSeeker.ts
+++ src/plugins/copyrightSeeker.ts
@@ -5,7 +5,7 @@ import { registerSongEvent } from "../api/song";
     // basically skips all empty samples
 */
 export const copyrightSeeker = () => {
-    const SAMPLE_THRESHOLD = 8
+    const SAMPLE_THRESHOLD = 10
     registerSongEvent("change:currentSound", async (song : {current?: any}) => {
         try {
             let body = await fetch(`${song?.current?.attributes?.waveform_url}`, {
@@ -14,6 +14,7 @@ export const copyrightSeeker = () => {
     
             const waveform : {width?: number, height?: number, samples?: number[]} = await body.json()
             let samples = waveform?.samples?.slice(0, waveform.samples.findIndex(value => value > SAMPLE_THRESHOLD)) ?? []
+            console.log(samples.length, waveform.samples?.length, song?.current?.getFixedDuration())
             song?.current?.seek((samples.length / (waveform.samples?.length ?? 1)) * song?.current?.getFixedDuration())
         } catch (err) {
             console.error("meowcloud - couldn't fetch waveform :3")
