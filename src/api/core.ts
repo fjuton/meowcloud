5d2851cf1b77c5f3494fe62ac17368252e55471e
adi ✨ <63885722+fjuton@users.noreply.github.com>
2025-04-04 04:11:05 +0200

    fix(song): update to latest soundcloud build

--- src/api/core.ts
+++ src/api/core.ts
@@ -15,13 +15,7 @@ interface Meowcloud {
 export let meowcloud : Meowcloud = {}
 export const requireModule = async (id : number) : Promise<WebpackModule|undefined> => {
     return new Promise((resolve) => {
-        if (meowcloud.webpack?.c[id] != undefined) {
-            resolve(meowcloud?.webpack?.c[id])
-        } else {
-            setTimeout(async () => {
-                await requireModule(id)
-                resolve(meowcloud.webpack?.c[id])
-            }, 100);
-        }
+        const checkForModule = () => meowcloud.webpack?.c[id] === undefined ? setTimeout(checkForModule) : resolve(meowcloud.webpack?.c[id])
+        checkForModule()
     })
 }
--- src/api/song.ts
+++ src/api/song.ts
@@ -1,7 +1,10 @@
 import { requireModule } from "./core"
 
 export const registerSongEvent = async (eventName : string, callback : any) => {
-    let mod = await requireModule(26)
+    // to find this magic module need to look for 
+    // change:currentsound
+    // TODO: make this automatically fetch the module or something
+    let mod = await requireModule(21)
     mod?.exports?.on(eventName, (song : any) => {
         callback(song)
     })
